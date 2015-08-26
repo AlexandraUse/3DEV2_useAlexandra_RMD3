49b6da34394830fa3b8bacce650aff6939f04b15
Alexandra Usé <alexandra.use@student.howest.be>
2015-08-26 10:27:31 +0200

    changes for heroku

--- models/User.js
+++
@@ -1,9 +0,0 @@
-// User schema
-module.exports = function(mongoose){
-	var schema = mongoose.Schema({
-		username: {type: String, required: true, unique: true},
-		character: {type: String, required: true, unique: true}
-	});
-
-	return mongoose.model("User", schema);
-};
--- server.js
+++ server.js
@@ -10,17 +10,13 @@ var server = require('http').Server(app);
 //** CONFIG **//
 require("./config/middleware.js")(app, express);
 require("./config/handlebars.js")(app);
-var mongoose = require("./config/mongoose.js")();
 
 //** MODULES **//
 require("./modules/server.js")(server);
-var auth = require("./modules/auth.js");
 var util = require("./modules/util.js");
 
 //** ROUTES **//
 require("./controllers/pages.js")(app);
-require("./controllers/api.js")(app, mongoose.models);
-require("./controllers/auth.js")(app, auth, util, mongoose.models.User);
 
 server.listen(port, function() {
   console.log('Server listening at port', port, 'in', env, 'mode');
