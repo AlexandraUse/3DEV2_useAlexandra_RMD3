920a13ebebb2328b1ffc8b0da7e21d50b1a57df8
Alexandra Usé <alexandra.use@student.howest.be>
2015-08-27 05:03:00 +0200

    btn

--- _components/connecting-screen/app.js
+++ _components/connecting-screen/app.js
@@ -1,5 +1,9 @@
 /* jshint newcap: false */
 
+function hideIntro(){
+    selfConnecting
+}
+
 var selfConnecting;
 Polymer({
 
@@ -12,5 +16,8 @@ Polymer({
     },
     clickStart: function(e){
         console.log('Start button');
+        selfConnecting.$.connecting-screen.hide();
+        console.log(document.querySelector('content'));
+        hideIntro();
     }
 });
