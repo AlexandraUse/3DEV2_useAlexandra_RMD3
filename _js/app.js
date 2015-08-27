d5e6a5580682e8b2b441191cd28697b309538797
Alexandra Usé <alexandra.use@student.howest.be>
2015-08-27 08:29:18 +0200

    broadcast player

--- _js/app.js
+++ _js/app.js
@@ -22,7 +22,7 @@ requirejs([
     }));
     this.socket.on("player_connected", canvas_body.onClient.bind(this));
     this.socket.on("player_connected", own_team_info.onClient.bind(this));
-    this.socket.on("player_connected", connecting_screen.onClient.bind(this));
+    this.socket.on("player_broadcast_connected", connecting_screen.onClient.bind(this));
     this.socket.on("player_disconnected", connecting_screen.onDisconnectedClient.bind(this));
     this.socket.on("remove_player", (function(data){
         socket.emit("remove player", data);
