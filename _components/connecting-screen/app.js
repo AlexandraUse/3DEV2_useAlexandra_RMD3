0e194de34b299398df8dd3e97c3bb226de43fddd
Alexandra Usé <alexandra.use@student.howest.be>
2015-08-27 05:20:48 +0200

    btn changes

--- _components/connecting-screen/app.js
+++ _components/connecting-screen/app.js
@@ -4,15 +4,20 @@ var selfConnecting;
 Polymer({
 
     bush: undefined,
+    playersOnReady: false,
 
     ready: function(){
         selfConnecting = this;
     },
     onPlayersReady: function(){
+        selfConnecting.playersOnReady = true;
     },
     clickStart: function(e){
-        console.log('Start button');
-        document.querySelector('.connectingscreen').style.display = 'none';
-        document.querySelector('.content').style.display = 'inline';
+        if(selfConnecting.playersOnReady == true){
+            console.log('Start button');
+            document.querySelector('.connectingscreen').style.display = 'none';
+            document.querySelector('.content').style.display = 'inline';
+            selfConnecting.playersOnReady = false;
+        }
     }
 });
