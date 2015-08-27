13f8d9e271fce50f754451723e5a2824f067ba7d
Alexandra Usé <alexandra.use@student.howest.be>
2015-08-27 04:58:32 +0200

    start btn

--- _components/connecting-screen/app.js
+++ _components/connecting-screen/app.js
@@ -9,17 +9,8 @@ Polymer({
         selfConnecting = this;
     },
     onPlayersReady: function(){
-        console.log(selfConnecting.$.start);
-        console.log(selfConnecting.shadowRoot.querySelector('start'));
-        console.log(document.querySelector('start'));
-        console.log($('.start'));
-        $('.start').addEventListener('click', function(e) {
-            console.log('Start button');
-           
-        });
-
-        /*selfConnecting.$.start.addEventListener('click', function(e) {
-            console.log('Start button');
-        });*/
+    },
+    clickStart: function(e){
+        console.log('Start button');
     }
 });
